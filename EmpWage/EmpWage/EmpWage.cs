666850518ada5e468850be912edd90165ff75015
VikasBenki <vikasmsbenki@gmail.com>
2022-02-01 17:27:36 +0530

    adding using collection problem

--- EmpWage/EmpWage/EmpWage.cs
+++ EmpWage/EmpWage/EmpWage.cs
@@ -26,12 +26,6 @@ namespace EmpWage
         {
             this.totalWage = totalWage;
         }
-
-        //Method to show Total Wage of a company.
-        public string toString()
-        {
-            return "Total Wage of a Company: " + this.company + " is " + this.totalWage;
-        }
     }
 }
 
